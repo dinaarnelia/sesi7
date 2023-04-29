b986750d8ce5b57094cfbe595ade11ea053cb737
dinaarnelia <127836859+dinaarnelia@users.noreply.github.com>
2023-04-29 09:59:55 +0800

    upload project

---
+++ sesi7/konfigurasi.php
@@ -0,0 +1,7 @@
+<?php
+    //konfigurasi koneksi ke DBMS MySQL
+    define("DBHOST", "localhost");
+    define("DBUSER", "root");
+    define("DBPASS", "");
+    define("DBPORT", "3306");
+    define("DBNAME", "dbmahasiswa");
